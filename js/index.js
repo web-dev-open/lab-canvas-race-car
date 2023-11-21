6dd6c865be5b5312aa3df91ca99f0ffe007de74c
svei00 <79055634+svei00@users.noreply.github.com>
2023-11-20 19:58:06 -0800

    Iteration 4: Create obstacles

--- js/index.js
+++ js/index.js
@@ -67,6 +67,74 @@ function startGame() {
       }
     });
 
+    // *****************************
+    // Iteration 4: Create obstacles
+    // *****************************
+
+    // Array to store obstacles
+    const obstacles = [];
+
+    // Function to create a new obstacle
+    function createObstacle() {
+      const obstacleWidth = 50;
+      const obstacleHeight = 20;
+      const obstacleX = Math.random() * (canvas.width - obstacleWidth);
+      const obstacleY = 0;
+
+      obstacles.push({ x: obstacleX, y: obstacleY, width: obstacleWidth, height: obstacleHeight });
+    }
+
+    // Function to update obstacle positions
+    function updateObstacles() {
+      for (let i = 0; i < obstacles.length; i++) {
+        const obstacle = obstacles[i];
+        
+        // Adjust the speed of obstacles
+        obstacle.y += 5; 
+
+        // Remove obstacles when they go beyond the canvas
+        if (obstacle.y > canvas.height) {
+          obstacles.splice(i, 1);
+          
+          // Adjust the index after removing an obstacle
+          i--; 
+        }
+      }
+    }
+
+    // Function to draw obstacles
+    function drawObstacles() {
+
+      // Adjust obstacle color
+      ctx.fillStyle = "orange"; 
+
+      for (const obstacle of obstacles) {
+        ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+      }
+    }
+
+    // Interval to create new obstacles every 2 seconds
+    setInterval(() => {
+      createObstacle();
+    }, 2000);
+
+    // Initialize a variable to track the road movement
+    let roadSpeed = 0;
+
+    // Update the road position
+    const updateRoadPosition = () => {
+      roadSpeed = 2; // Adjust the speed of the road
+
+      // Move the road
+      ctx.drawImage(roadImg, 0, roadSpeed, canvas.width, canvas.height);
+      ctx.drawImage(roadImg, 0, -canvas.height + roadSpeed, canvas.width, canvas.height);
+
+      // Reset the road position
+      if (roadSpeed >= canvas.height) {
+        roadSpeed = 0;
+      }
+    };
+
     // Updating the car position
     const updateCarPosition = () => {
       carX += carSpeed;
@@ -79,14 +147,29 @@ function startGame() {
       }
     };
 
-    // Adding animation loop
-    function drawGame() {
+    // Check collisions with obstacles
+    for (const obstacle of obstacles) {
+      if (
+        carX < obstacle.x + obstacle.width &&
+        carX + carWidth > obstacle.x &&
+        carY < obstacle.y + obstacle.height &&
+        carY + carHeight > obstacle.y
+      ) {
+    // Collision detected, you can add your game over logic here
+    console.log("Game Over!");
+    }
+  }
+
+    // Adding animation loop then Modify the drawGame 
+    const drawGame = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+      updateRoadPosition(); // Move the road
       updateCarPosition();
+      updateObstacles(); // Update obstacle positions
+      drawObstacles(); // Draw obstacles
       ctx.drawImage(carImg, carX, carY, carWidth, carHeight);
       requestAnimationFrame(drawGame);
-    }
+    };
 
     drawGame();
   };
