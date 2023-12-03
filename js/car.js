04215681ef06806fbe5121d1b6f62e2aa24f08f9
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-12-03 13:10:14 +0530

    task completed

---
+++ js/car.js
@@ -0,0 +1,25 @@
+class Car {
+  constructor() {
+    this.x = 225;
+    this.y = 500;
+    this.width = 50;
+    this.height = 100;
+    this.speed = 5;
+    this.carImg = new Image();
+    this.carImg.src = "./images/car.png";
+  }
+
+  draw() {
+    ctx.drawImage(this.carImg, this.x, this.y, this.width, this.height);
+  }
+
+  moveLeft() {
+    if (this.x <= 75) return;
+    this.x -= 25;
+  }
+
+  moveRight() {
+    if (this.x >= 375) return;
+    this.x += 25;
+  }
+}
